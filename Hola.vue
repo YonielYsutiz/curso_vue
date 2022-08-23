5927537b125f885bbaeb0d4bbe2913658aa0bf2a
Yoniel Ysturiz <ysturizyoniel@gmail.com>
2022-08-23 00:38:14 -0500

    Curso de vue basico terminado

---
+++ Hola.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Amante de la coca</p>
+    <div>{{ text }}</div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                text:"Hola!"
+            }
+        }
+    }
+</script>
+
+<style>
+p {
+    color: red;
+}
+</style>
